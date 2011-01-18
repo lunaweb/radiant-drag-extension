00c1bf2c7b7757356e5739daa1037fa2740cc0e7
Aimerick Desdoit <aimerick@lunaweb.fr>
2011-01-18 15:31:58 +0100

    avant de sauvegarder la page, s'il s'agit d'un nouvel enregistrement on va chercher la position maximale pour éviter d'avoir une position à nil ou 0

--- lib/drag/models/page.rb
+++ lib/drag/models/page.rb
@@ -10,6 +10,20 @@ module Drag
             Page::NONDRAFT_FIELDS << 'position'
           end
         end
+        base.send :include, InstanceMethods
+        base.before_save :max_order
+      end
+      
+      module InstanceMethods
+        def max_order
+          return unless new_record?
+          maxorder = self.class.find_last_by_parent_id parent_id, :order => ["position ASC"]
+          if maxorder
+            self.position = maxorder.position + 1
+          else
+            self.position = 1
+          end
+        end
       end
       
     end
